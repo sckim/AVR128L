7de60461a6429611b6db8cea0f2155d9a643eee7
sckim <dr.soochankim@gmail.com>
2017-11-08 19:57:37 +0900

    Update main.c

--- 82_PWM/main.c
+++ 82_PWM/main.c
@@ -3,6 +3,8 @@
  *
  *  Created on: 2017. 10. 27.
  *      Author: Soochan Kim
+ * PORTD ì¶”ê°€
+ * PWM ì¥ì 
  */
 
 #include <avr/io.h>
@@ -19,7 +21,7 @@ unsigned char text[] = "\r\nWelcome!\r\n";
 #endif
 
 void putch(unsigned char data) {
-	//Àü¼ÛÁØºñ°¡ µÉ ¶§±îÁö ´ë±â
+	//ì „ì†¡ì¤€ë¹„ê°€ ë  ë•Œê¹Œì§€ ëŒ€ê¸°
 	while ((UCSR0A & (1 << UDRE0)) == 0)
 		;
 	// while(!(UCSR0A & 0x20)) ;
@@ -29,7 +31,7 @@ void putch(unsigned char data) {
 
 void printfStr(char data, FILE * stream) {
 //void putch(unsigned char data) {
-	//Àü¼ÛÁØºñ°¡ µÉ ¶§±îÁö ´ë±â
+	//ì „ì†¡ì¤€ë¹„ê°€ ë  ë•Œê¹Œì§€ ëŒ€ê¸°
 	while ((UCSR0A & (1 << UDRE0)) == 0)
 		;
 	// while(!(UCSR0A & 0x20)) ;
@@ -40,14 +42,14 @@ void printfStr(char data, FILE * stream) {
 unsigned char getch() {
 	unsigned char data;
 
-	//µ¥ÀÌÅÍ¸¦ ¹ŞÀ» ¶§±îÁö ´ë±â
+	//ë°ì´í„°ë¥¼ ë°›ì„ ë•Œê¹Œì§€ ëŒ€ê¸°
 	while ((UCSR0A & (1 << RXC0)) == 0)
 		;
 
 	//while (bit_is_set(UCSR0A,RXC0);
 	//while (!(UCSR0A & 0x80);
 
-	// Receiver register¿¡ º¸³»°íÀÚ ÇÏ´Â µ¥ÀÌÅÍ¸¦ ÀúÀå
+	// Receiver registerì— ë³´ë‚´ê³ ì í•˜ëŠ” ë°ì´í„°ë¥¼ ì €ì¥
 	data = UDR0;
 
 	return data;
@@ -58,24 +60,24 @@ unsigned char getch() {
 
 void uart_init(unsigned long iBaudrate) {
 
-	// UCSRnA ·¹Áö½ºÅÍ¸¦ ÃÊ±âÈ­½ÃÅ²´Ù.
-	// 0¹øÂ° ºñÆ®, Áï MPCMn ¸¦ 0À¸·Î ¼¼Æ® (USARTnÀ» ¸ÖÆ¼ ÇÁ·Î¼¼¼­ Åë½Å¸ğµå·Î ¼³Á¤)
+	// UCSRnA ë ˆì§€ìŠ¤í„°ë¥¼ ì´ˆê¸°í™”ì‹œí‚¨ë‹¤.
+	// 0ë²ˆì§¸ ë¹„íŠ¸, ì¦‰ MPCMn ë¥¼ 0ìœ¼ë¡œ ì„¸íŠ¸ (USARTnì„ ë©€í‹° í”„ë¡œì„¸ì„œ í†µì‹ ëª¨ë“œë¡œ ì„¤ì •)
 	UCSR0A = 0x00;
 
-	// UCSRnB ·¹Áö½ºÅÍ¸¦ ÀÌ¿ëÇÏ¿© ¼Û½Å ¹× ¼ö½Å »ç¿ë¼³Á¤À» ÇÑ´Ù.
+	// UCSRnB ë ˆì§€ìŠ¤í„°ë¥¼ ì´ìš©í•˜ì—¬ ì†¡ì‹  ë° ìˆ˜ì‹  ì‚¬ìš©ì„¤ì •ì„ í•œë‹¤.
 	// Rx, Tx enable
 	UCSR0B = (1 << RXEN0) | (1 << TXEN0);
 
-	// 3¹øÂ°, 4¹øÂ° ºñÆ® ¼¼Æ® Áï, TXENn (USARTn¸ğµâÀÇ ¼Û½ÅºÎ µ¿ÀÛ enable) RXENn (USARTn¸ğµâÀÇ ¼ö½ÅºÎ µ¿ÀÛ enable)
-	//  2¹ø ºñÆ® UCSZ02 = 0À¸·Î ¼¼Æ®
+	// 3ë²ˆì§¸, 4ë²ˆì§¸ ë¹„íŠ¸ ì„¸íŠ¸ ì¦‰, TXENn (USARTnëª¨ë“ˆì˜ ì†¡ì‹ ë¶€ ë™ì‘ enable) RXENn (USARTnëª¨ë“ˆì˜ ìˆ˜ì‹ ë¶€ ë™ì‘ enable)
+	//  2ë²ˆ ë¹„íŠ¸ UCSZ02 = 0ìœ¼ë¡œ ì„¸íŠ¸
 
-	// UCRnC ·¹Áö½ºÅÍ¸¦ ÀÌ¿ëÇÏ¿© ¸ğµå(µ¿±â/ºñµ¿±â), ÆĞ¸®Æ¼¸ğµå, Á¤ÁöºñÆ®,
-	// Àü¼Û µ¥ÀÌÅÍ ºñÆ®¼ö¸¦ ¼³Á¤ÇÑ´Ù.
-	// ºñµ¿±â ¹æ½Ä, No Parity bit, 1 Stop bit, 8bits
+	// UCRnC ë ˆì§€ìŠ¤í„°ë¥¼ ì´ìš©í•˜ì—¬ ëª¨ë“œ(ë™ê¸°/ë¹„ë™ê¸°), íŒ¨ë¦¬í‹°ëª¨ë“œ, ì •ì§€ë¹„íŠ¸,
+	// ì „ì†¡ ë°ì´í„° ë¹„íŠ¸ìˆ˜ë¥¼ ì„¤ì •í•œë‹¤.
+	// ë¹„ë™ê¸° ë°©ì‹, No Parity bit, 1 Stop bit, 8bits
 	UCSR0C |= (1 << UCSZ01);
 	UCSR0C |= (1 << UCSZ00);
 	/*
-	 // UBRRnH(L) ·¹Áö½ºÅÍ¸¦ ÀÌ¿ëÇÑ ¼Û¼ö½Å º¸·¹ÀÌÆ® ¼³Á¤
+	 // UBRRnH(L) ë ˆì§€ìŠ¤í„°ë¥¼ ì´ìš©í•œ ì†¡ìˆ˜ì‹  ë³´ë ˆì´íŠ¸ ì„¤ì •
 	 UBRR0H = 0x00;
 	 switch (iBaudrate) {
 	 case 9600:
